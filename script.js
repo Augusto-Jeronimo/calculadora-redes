1b4cec78a4b14f5dff2d2affacb72004b8f90c83
Augusto Jerônimo <dev.augustoj@gmail.com>
2023-08-05 19:58:04 -0300

    Adição do script de execução do calculo

--- script.js
+++ script.js
@@ -10,3 +10,32 @@ function opcMark(clicado, n) {
         clicado.style.backgroundColor = 'var(--normal1)'
     }
 }
+
+/* --v-- Códogo de seleção --v-- */
+
+function enviar(opc) {
+    var metodo = document.getElementsByName('opc')
+
+    if (document.getElementById('codigo').value.length == 0) {
+        
+        alert('[ERRO] O campo não pode estar vazio!')
+
+    }else if (metodo[0].checked) {
+        
+        crc(opc)
+
+    }else if (metodo[1].checked) {
+
+        checksum(opc)
+
+    }else if (metodo[2].checked) {
+
+        bitDeParidade(opc)
+
+    }else {
+
+        alert('[ERRO] Escolha uma das opções!')
+
+    }
+    
+}
